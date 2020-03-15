e6e3322831321b40d2c16e0eef25a23a3f92105b
Howard <hh680821@163.com>
2020-03-15 12:38:42 +0800

    Initial Commit

---
+++ SwiftUIReference/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIReference
+//
+//  Created by Howard Ge on 2020/3/15.
+//  Copyright © 2020 Howard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
